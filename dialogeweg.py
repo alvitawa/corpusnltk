251b7a3f027b8ab2773d0c3e1c1dd351e071575f
exempll <alfonsotabo@gmail.com>
2019-05-08 11:45:40 +0200

    corpusc

--- dialogeweg.py
+++ dialogeweg.py
@@ -2,10 +2,19 @@
 
 with open('in.txt', 'r') as in_file, open('out.txt', 'w') as out_file:
     content = in_file.read()
+    print('Original: ~%s' % len(content.split(' ')))
     text = ''
     for sentence in content.split('.'):
-        if sentence.count("\"") < 1:
-            text += sentence + '.'
+        if '\'' in sentence:
+            continue
+        if '\"' in sentence:
+            continue
+        if '-' in sentence: 
+            continue
+        if '.' in sentence:
+            continue
+        no_commas = ''.join(sentence.split(','))
+        text += sentence + '. '
         if len(text.split(' ')) > 1000:
             break
     out_file.write(text)
