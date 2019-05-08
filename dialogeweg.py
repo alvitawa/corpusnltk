69d21691a222b180f61958f14d54c3c41b3416eb
exempll <alfonsotabo@gmail.com>
2019-05-08 12:57:15 +0200

    l

--- dialogeweg.py
+++ dialogeweg.py
@@ -13,8 +13,13 @@ with open('in.txt', 'r') as in_file, open('out.txt', 'w') as out_file:
             continue
         if '.' in sentence:
             continue
-        no_commas = ''.join(sentence.split(','))
-        text += no_commas + '. '
+        if '(' in sentence or ')' in sentence:
+            continue
+        # no_commas = ''.join(sentence.split(','))
+        stripped = sentence.strip()
+        if stripped == '':
+            continue
+        text += stripped + '. ' #no_commas + '. '
         if len(text.split(' ')) > 1000:
             break
     out_file.write(text)
---
+++ lexicon.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import nltk
+
+with open('out.txt', 'r') as in_file, open('lexicon.txt', 'w') as lexicon_file:
+    corpus = in_file.read()
+    atoms = set(nltk.word_tokenize(corpus))
+    lexicon = nltk.pos_tag(atoms)
+
+    str_lexicon = ''
+    for pair in sorted(tuple(reversed(pair)) for pair in lexicon):
+        str_lexicon += ' '.join(pair) + '\n'
+    lexicon_file.write(str_lexicon)
